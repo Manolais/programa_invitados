3186922a3943f0bccb48fe39376a6d7e5913a461
Manolais <bsasmanuelsilva@gmail.com>
2021-01-23 13:55:35 -0300

    Opción 1 y personas.txt

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import smtplib as sm
 import os
 
+personas = []
+
+txt = open('personas.txt','a')
+
 while True:
     os.system('cls')
     print('1 - Ingreso')
     print('2 - Ver cantidad de personas')
-    print('3 - Salir')
+    print('3 - Salir y enviar')
     n = input('>>>')
     if n == '1':
-        os.system('cls')
-        print('Opción 1')
-        input('presione enter para continuar...')
+        while True:
+            os.system('cls')
+            per = input('Ingrese el nombre: ')
+            tel = input('Ingrese número telefónico: ')
+            try:
+                tel = int(tel)
+                break
+            except ValueError:
+                print('Ingrese un valor numérico')
+                input('Presione enter para continuar...')
+        mail = input('Ingrese el mail:')
+                
+
     elif n == '2':
         os.system('cls')
         print('Opcion 2')
