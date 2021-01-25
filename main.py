f83b28ce4aa1de1cca87efa61fc92d313537b054
Manolais <bsasmanuelsilva@gmail.com>
2021-01-25 17:42:35 -0300

    x

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ while True:
                 print('Ingrese un valor numérico')
                 input('Presione enter para continuar...')
         mail = input('Ingrese el mail: ')
-        personas.append({'Nombre': per,'Teléfono': tel,'Email': mail})
+        personas.append({'Nombre': per,'Telefono': tel,'Email': mail})
+        print(personas)
         print('¡Invitado ingresado correctamente!')
         input('Presione enter para continuar...')
 
@@ -38,14 +39,17 @@ while True:
                     print('')
                     print('Invitado',i+1)
                     print()
-                    print('Nombre:',personas[i]['Nombre'])
-                    print('Teléfono:',personas[i]['Teléfono'])
-                    print('Email:',personas[i]['Email'])
+                    print('Nombre:  ',personas[i]['Nombre'])
+                    print('Teléfono:',personas[i]['Telefono'])
+                    print('Email:   ',personas[i]['Email'])
                     print('--------------------')
                 input('Presione enter para continuar')
             if p == '2':
                 wb = load_workbook('personas.xlsx')
                 sheet1 = wb.get_sheet_by_name('Sheet1')
+                sheet1['A1'] = 'Nombre'
+                sheet1['B1'] = 'Teléfono'
+                sheet1['C1'] = 'Email'
                 if type(sheet1['A2'].value) == None:
                     os.system('cls')
                     print('Excel vacio, vuelva cuando guarde los cambios')
@@ -56,9 +60,9 @@ while True:
                     for i in range(len(sheet1['A'])-1):
                         print('Invitado', i+1)
                         print()
-                        print(sheet1['A'+str(i+2)].value)
-                        print(sheet1['B'+str(i+2)].value)
-                        print(sheet1['C'+str(i+2)].value)
+                        print('Nombre:  ',sheet1['A'+str(i+2)].value)
+                        print('Teléfono:',sheet1['B'+str(i+2)].value)
+                        print('Email:   ',sheet1['C'+str(i+2)].value)
                         print('--------------------')
                     input('Presione enter para continuar...')
             break
@@ -67,11 +71,15 @@ while True:
     elif n == '3':
         wb = load_workbook('personas.xlsx')
         sheet = wb.get_sheet_by_name('Sheet1')
+        sheet['A1'] = 'Nombre'
+        sheet['B1'] = 'Teléfono'
+        sheet['C1'] = 'Email'
         for i in range(len(personas)):
             sheet['A'+str(len(sheet['A'])+1)] = personas[i]['Nombre']
-            sheet['B'+str(len(sheet['B']))] = personas[i]['Teléfono']
+            sheet['B'+str(len(sheet['B']))] = personas[i]['Telefono']
             sheet['C'+str(len(sheet['C']))] = personas[i]['Email']
         wb.save('personas.xlsx')
+        os.system('cls')
         sv = sm.SMTP('smtp.gmail.com',587)
         sv.starttls()
         sv.login('programainvitados@gmail.com','prog_invit')
@@ -79,6 +87,9 @@ while True:
         os.system('cls')
         print('Gracias por usar el programa, guardando invitados...')
         print('Correo enviado correctamente!')
+        input('Presione enter para salir...')
+        exit()
+    elif n=='exit()':
         exit()
     else:
         print('Ingrese una opción valida')
